341b5fb757edb4161677efb03f4ef7c964bb1651
Hugo Herter <git@hugoherter.com>
2015-08-04 19:42:56 +0200

    Add setup_salt.sh script

---
+++ setup_salt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# --- Ubuntu ---
+
+# Install the Salt Stack PPA repo and salt-minion:
+
+echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | tee /etc/apt/sources.list.d/saltstack.list
+wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | sudo apt-key add -
+
+apt-get update
+apt-get install salt-minion
