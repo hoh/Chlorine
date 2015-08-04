3a5520f031a0a6ca36cd0380ecbae937f0a92a06
Hugo Herter <git@hugoherter.com>
2015-08-04 21:11:38 +0200

    Fix missing 'wget' in setup_salt

--- setup_salt.sh
+++ setup_salt.sh
@@ -2,6 +2,11 @@
 
 # --- Ubuntu ---
 
+# Install wget if it is missing, such as in LXC Ubuntu templates:
+
+dpkg -s wget 2>/dev/null >/dev/null || apt-get update && sudo apt-get -y install wget
+
+
 # Install the Salt Stack PPA repo and salt-minion:
 
 echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | tee /etc/apt/sources.list.d/saltstack.list
